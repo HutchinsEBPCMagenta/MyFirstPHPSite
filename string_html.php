c7aa52f886b2a9397bd751935cb71bc8e6736ab7
Ellie Hutchins <32098835+HutchinsEBPCMagenta@users.noreply.github.com>
2017-10-17 14:42:23 +0100

    Add files via upload

---
+++ string_html.php
@@ -0,0 +1,13 @@
+<?php require_once 'index.php';?>
+
+<html>
+	<head>
+	</head>
+	<body>
+		I have found <?php echo $myfirst; ?>.<br />
+		I have found <?php echo $firstfloat; ?>.<br />
+		I have found <?php echo $fullname ; ?>.<br />
+		I have found <?php echo $age; ?>.<br />
+		I have found <?php echo $dob; ?>.<br />
+	</body>
+</html>
