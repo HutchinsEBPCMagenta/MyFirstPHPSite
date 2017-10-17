8e977b42328ee7160fb0026ee8c3eeaa1c54cd51
Ellie Hutchins <32098835+HutchinsEBPCMagenta@users.noreply.github.com>
2017-10-17 13:45:17 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<HTML>
+
+<BODY>
+
+<?php
+
+$myfirst = "Hello world!";
+$firstnum = 4;
+$firstfloat = 24.2;
+
+echo $myfirst;
+echo "<br>";
+echo $firstnum;
+echo "<br>";
+echo $firstfloat;
+
+?>
+
+</BODY>
+
+</HTML>
